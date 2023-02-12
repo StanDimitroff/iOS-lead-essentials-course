eec165aaf8430a3f2195498ed434388a3b8e407f
standimitroff <standimitroff@gmail.com>
2023-02-12 17:18:29 +0200

    Move `URLSessionHTTPClient` creation to a factory `makeSUT` method

--- EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class URLSessionHTTPCLient {
+class URLSessionHTTPClient {
   private let session: URLSession
 
   init(session: URLSession = .shared) {
@@ -49,7 +49,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
       expectation.fulfill()
     }
 
-    URLSessionHTTPCLient().get(from: url, completion: { _ in })
+    makeSUT().get(from: url, completion: { _ in })
     wait(for: [expectation], timeout: 1.0)
   }
 
@@ -60,11 +60,9 @@ final class URLSessionHTTPClientTests: XCTestCase {
     let error = NSError(domain: "Some error", code: 1)
     URLProtocolStub.stub(data: nil, response: nil, error: error)
 
-    let sut = URLSessionHTTPCLient()
-
     let expectation = expectation(description: "Wait for completion")
 
-    sut.get(from: url) { result in
+    makeSUT().get(from: url) { result in
       switch result {
         case let .failure(receivedError as NSError):
           XCTAssertEqual(receivedError.domain, error.domain)
@@ -80,6 +78,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
   }
 
   // MARK - Helpers
+
+  private func makeSUT() -> URLSessionHTTPClient {
+    URLSessionHTTPClient()
+  }
 
   private class URLProtocolStub: URLProtocol {
     private static var stub: Stub?
