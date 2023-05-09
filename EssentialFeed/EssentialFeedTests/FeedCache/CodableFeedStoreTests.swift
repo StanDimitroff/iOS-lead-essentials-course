9b9086f3280a7ce9f6babe5ed92a48cbc9ef6ee4
standimitroff <standimitroff@gmail.com>
2023-05-09 09:56:30 +0300

    Extract `storeURL` creation into a factory method

--- EssentialFeed/EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -67,15 +67,13 @@ final class CodableFeedStoreTests: XCTestCase {
   override func setUp() {
     super.setUp()
 
-    let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
-    try? FileManager.default.removeItem(at: storeURL)
+    try? FileManager.default.removeItem(at: storeURL())
   }
 
   override func tearDown() {
     super.tearDown()
 
-    let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
-    try? FileManager.default.removeItem(at: storeURL)
+    try? FileManager.default.removeItem(at: storeURL())
   }
 
   func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -146,11 +144,14 @@ final class CodableFeedStoreTests: XCTestCase {
   }
 
   private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
-    let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
-    let sut = CodableFeedStore(storeURL: storeURL)
+    let sut = CodableFeedStore(storeURL: storeURL())
 
     trackForMemoryLeaks(for: sut, file: file, line: line)
 
     return sut
+  }
+
+  private func storeURL() -> URL {
+    FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
   }
 }
