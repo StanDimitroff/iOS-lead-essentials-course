cbde898fbcb9368556ca988686db2ca76fbe2245
standimitroff <standimitroff@gmail.com>
2023-05-16 20:11:24 +0300

    Insert to non-empty cache overrides previously inserted cache values

--- EssentialFeed/EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -130,6 +130,21 @@ final class CodableFeedStoreTests: XCTestCase {
     expect(sut, toRetrieveTwice: .failure(anyNSError()))
   }
 
+  func test_insert_overridesPreviouslyInsertedCacheValues() {
+    let sut = makeSUT()
+
+    let firstInsertionError = insert(cache: (feed: uniqueImageFeed().local, timestamp: Date()), to: sut)
+    XCTAssertNil(firstInsertionError)
+
+    let latestFeed = uniqueImageFeed().local
+    let latestTimestamp = Date()
+
+    let secondInsertionError = insert(cache: (feed: latestFeed, timestamp: latestTimestamp), to: sut)
+    XCTAssertNil(secondInsertionError)
+
+    expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
+  }
+
   // MARK: - Helpers
 
   private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
@@ -140,13 +155,17 @@ final class CodableFeedStoreTests: XCTestCase {
     return sut
   }
 
-  private func insert(cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) {
+  @discardableResult
+  private func insert(cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) -> Error? {
     let exp = expectation(description: "Wait for cache insertion")
-    sut.insert(cache.feed, timestamp: cache.timestamp) { insertionError in
-      XCTAssertNil(insertionError, "Expected feed to be inserted succefully.")
+    var insertionError: Error?
+    sut.insert(cache.feed, timestamp: cache.timestamp) { receivedInsertionError in
+      insertionError = receivedInsertionError
       exp.fulfill()
     }
     wait(for: [exp], timeout: 1.0)
+
+    return insertionError
   }
 
   private func expect(_ sut: CodableFeedStore, toRetrieve expectedResult:
