d8b91c5f1aac215fbb264a085a281e8c014bde51
standimitroff <standimitroff@gmail.com>
2023-08-30 16:39:18 +0300

    Extract `cancelImageDataLoad` from `FeedImageDataLoader` protocol into a new `FeedImageDataLoaderTask` protocol that is a task that can be cancelled. Implementations of `FeedImageDataLoader` are not forced to be stateful.

--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 import EssentialFeed
 
+public protocol FeedImageDataLoaderTask {
+  func cancel()
+}
+
 public protocol FeedImageDataLoader {
-  func loadFeedImageData(from url: URL)
-  func cancelImageDataLoad(from url: URL)
+  func loadFeedImageData(from url: URL) -> FeedImageDataLoaderTask
 }
 
 final public class FeedViewController: UITableViewController {
@@ -18,6 +21,8 @@ final public class FeedViewController: UITableViewController {
   private var feedLoader: FeedLoader?
   private var imageLoader: FeedImageDataLoader?
   private var tableModel = [FeedImage]()
+
+  private var tasks = [IndexPath: FeedImageDataLoaderTask]()
 
   public convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
     self.init()
@@ -55,14 +60,13 @@ final public class FeedViewController: UITableViewController {
     cell.locationLabel.text = cellModel.location
     cell.descriptionLabel.text = cellModel.description
 
-    imageLoader?.loadFeedImageData(from: cellModel.url)
+    tasks[indexPath] = imageLoader?.loadFeedImageData(from: cellModel.url)
 
     return cell
   }
 
-  public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-    let cellModel = tableModel[indexPath.row]
-
-    imageLoader?.cancelImageDataLoad(from: cellModel.url)
+  public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) { 
+    tasks[indexPath]?.cancel()
+    tasks[indexPath] = nil
   }
 }
