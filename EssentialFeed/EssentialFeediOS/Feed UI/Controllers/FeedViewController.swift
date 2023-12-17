e9bcbc7e185262e9779721cd46022d61abd77a29
standimitroff <standimitroff@gmail.com>
2023-12-17 22:10:50 +0200

    `ErrorView` renders error message on load feed error

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -15,7 +15,7 @@ public final class ErrorView: UIView {
   public var message: String?
 }
 
-public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView {
+public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
 
   var delegate: FeedViewControllerDelegate?
   public let errorView = ErrorView()
@@ -38,6 +38,10 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     } else {
       refreshControl?.endRefreshing()
     }
+  }
+
+  func display(_ viewModel: FeedErrorViewModel) {
+    errorView.message = viewModel.message
   }
 
   @IBAction private func refresh() {
--- EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
@@ -24,14 +24,24 @@ protocol FeedView {
   func display(_ viewModel: FeedViewModel)
 }
 
+struct FeedErrorViewModel {
+  let message: String
+}
+
+protocol FeedErrorView {
+  func display(_ viewModel: FeedErrorViewModel)
+}
+
 final class FeedPresenter {
 
   private let loadingView: FeedLoadingView
   private let feedView: FeedView
+  private let errorView: FeedErrorView
 
-  init(loadingView: FeedLoadingView, feedView: FeedView) {
+  init(loadingView: FeedLoadingView, feedView: FeedView, errorView: FeedErrorView) {
     self.loadingView = loadingView
     self.feedView = feedView
+    self.errorView = errorView
   }
 
   static var title: String {
@@ -40,6 +50,13 @@ final class FeedPresenter {
       tableName: "Feed",
       bundle: .init(for: FeedPresenter.self),
       comment: "Title for the feed view")
+  }
+
+  private var feedLoadError: String {
+    return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
+                             tableName: "Feed",
+                             bundle: Bundle(for: FeedPresenter.self),
+                             comment: "Error message displayed when we can't load the image feed from the server")
   }
 
   func didStartLoadingFeed() {
@@ -52,6 +69,7 @@ final class FeedPresenter {
   }
 
   func didEndLoadingFeed(with error: Error) {
+    errorView.display(FeedErrorViewModel(message: feedLoadError))
     loadingView.display(FeedLoadingViewModel(isLoading: false))
   }
 }
