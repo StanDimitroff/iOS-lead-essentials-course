1ad499db1b4fcb624ec1732caa2b89fd68a7008e
standimitroff <standimitroff@gmail.com>
2024-02-13 10:32:39 +0200

    Move `FeedLoadingViewModel` to a separate  ile

---
+++ EssentialFeed/EssentialFeed/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Stanislav Dimitrov on 13.02.24.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+  public let isLoading: Bool
+}
