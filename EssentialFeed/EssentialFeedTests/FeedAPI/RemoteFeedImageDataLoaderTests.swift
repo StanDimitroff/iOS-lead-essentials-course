5c00646fa31817359d096fa3332523c4508b2b31
standimitroff <standimitroff@gmail.com>
2024-10-14 09:01:21 +0300

    Remove `@discardableResult` attribute — `FeedImageDataLoaderTask` instances should be properly managed and never discarded.

--- EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
@@ -20,7 +20,7 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
     let url = URL(string: "https://a-url.com")!
     let (sut, client) = makeSUT()
 
-    sut.loadImageData(from: url) { _ in }
+    _ = sut.loadImageData(from: url) { _ in }
 
     XCTAssertEqual(client.requestedURLs, [url])
   }
@@ -29,8 +29,8 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
     let url = URL(string: "https://a-url.com")!
     let (sut, client) = makeSUT()
 
-    sut.loadImageData(from: url) { _ in }
-    sut.loadImageData(from: url) { _ in }
+    _ = sut.loadImageData(from: url) { _ in }
+    _ = sut.loadImageData(from: url) { _ in }
 
     XCTAssertEqual(client.requestedURLs, [url, url])
   }
@@ -108,7 +108,7 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
     var sut: RemoteFeedImageDataLoader? = RemoteFeedImageDataLoader(client: client)
 
     var capturedResults = [FeedImageDataLoader.Result]()
-    sut?.loadImageData(from: anyURL()) { capturedResults.append($0) }
+    _ = sut?.loadImageData(from: anyURL()) { capturedResults.append($0) }
 
     sut = nil
     client.complete(withStatusCode: 200, data: anyData())
@@ -130,7 +130,7 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
   private func expect(_ sut: RemoteFeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
     let exp = expectation(description: "Wait for load completion")
 
-    sut.loadImageData(from: anyURL()) { receivedResult in
+    _ = sut.loadImageData(from: anyURL()) { receivedResult in
       switch (receivedResult, expectedResult) {
       case let (.success(receivedData), .success(expectedData)):
         XCTAssertEqual(receivedData, expectedData, file: file, line: line)
